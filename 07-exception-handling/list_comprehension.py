268f99813da7a8021c22bc5a4d6a6895dc87dd5a
muhammadwali0 <waleeraxa@gmail.com>
2026-03-12 14:58:18 +0500

    feat: add a program to handle exceptions in list comprehension

---
+++ 07-exception-handling/list_comprehension.py
@@ -0,0 +1,2 @@
+## Program to write a function that uses a list comprehension to convert a list of strings to integers. Use try, except, and finally blocks within the list comprehension to handle ValueError and print an appropriate message.
+
