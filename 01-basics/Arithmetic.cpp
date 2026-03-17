d4ed5294a29c7a2c6b74cb36d0e92e1453a536ae
muhammadwali0 <waleeraxa@gmail.com>
2026-03-17 17:25:50 +0500

    feat: add a program to perform basic arithmetic

---
+++ 01-basics/Arithmetic.cpp
@@ -0,0 +1,17 @@
+// Program to perform arithmetic functions
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  float a = 2;
+  float b = 4;
+
+  cout << a + b << "\n";
+  cout << a - b << "\n";
+  cout << a * b << "\n";
+  cout << a / b << "\n";
+
+  return 0;
+}
