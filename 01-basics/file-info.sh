8db954f08745e5b3e30bed0fc8afd70e0bf0bcad
muhammadwali0 <waleeraxa@gmail.com>
2026-03-19 04:04:38 +0500

    refactor: replaced pwd with PWD, basename with faster shortcut and quoted assignments

--- 01-basics/file-info.sh
+++ 01-basics/file-info.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # script that prints its own filename, the current user, and the current directory
 
-filename=$(basename "$0")
-echo "'$USER' executed '$filename' in '$(pwd)'"
+filename="${0##*/}"
+echo "'$USER' executed '$filename' in '${PWD}'"
