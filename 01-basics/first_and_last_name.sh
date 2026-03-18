8c1385900e075744f43c38c41361af96956a0646
muhammadwali0 <waleeraxa@gmail.com>
2026-03-18 22:50:14 +0500

    feat: add a program to take in two inputs

---
+++ 01-basics/first_and_last_name.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#script that accepts a first and last name as arguments and prints the full name
+
+read -p "Enter your first name: " fname
+read -p "Enter your last name: " lname
+
+echo "Your full name is $fname $lname"
