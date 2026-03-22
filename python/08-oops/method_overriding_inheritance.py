a37da736a27e98deb4f4a5e82a17a827bd114b3c
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-03-22 19:14:43 +0500

    feat: add a program to use method overriding in single inheritance

---
+++ python/08-oops/method_overriding_inheritance.py
@@ -0,0 +1,24 @@
+## Program to create a base class with attributes and a derived class that inherits from base class and adds an attribute and an object of the child class and print its attributes. in the class, override the `__str__` method to return a string representation of the object
+
+
+class Animal:
+    def __init__(self, name: str, species: str) -> None:
+        self.name = name
+        self.species = species
+
+
+class Dog(Animal):
+    def __init__(self, name: str, species: str, breed: str) -> None:
+        super().__init__(name, species)
+        self.breed = breed
+
+    def __str__(self) -> str:
+        return f"Type: Dog\nName: {self.name}\nSpecies: {self.species}\nBreed: {self.breed}"
+
+
+my_pet = Dog("Bruno", "Canis lupus familiaris", "Norwegian Lundehund")
+
+print(
+    f"My pet's name is {my_pet.name}, it's species is {my_pet.species} and it's breed is {my_pet.breed}."
+)
+print(my_pet)
