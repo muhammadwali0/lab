50e4a8fbd6c6f3adc7b91dc7c441cff4431b2e0e
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-04-07 02:12:54 +0500

    feat: add a program to print even numbers using a for loop

---
+++ C/03-loops/evennumbers.c
@@ -0,0 +1,11 @@
+// Program to print the first 10 even numbers using a for loop
+
+#include <stdio.h>
+
+int main() {
+    printf("First 10 even numbers:\n");
+    for (int i = 0; i <= 18; i += 2) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
