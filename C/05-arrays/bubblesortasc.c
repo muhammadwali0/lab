3e72320503b90d1b504a638b58c13274dc2c8613
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-04-07 02:26:27 +0500

    feat: add a program to sort an array in ascending order using bubble sort

---
+++ C/05-arrays/bubblesortasc.c
@@ -0,0 +1,25 @@
+// Program to sort an array in ascending order using the Bubble Sort algorithm
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[100];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+    printf("Sorted array (ascending): ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
