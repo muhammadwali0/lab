d0243b27a0dd2cff691e6836716fb28159843152
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-04-07 02:35:57 +0500

    feat: add a program to print each character of a string on a new line

---
+++ C/09-strings/charprinter.c
@@ -0,0 +1,13 @@
+// Program to print each character of a string on a separate line using a loop
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("Characters:\n");
+    for (int i = 0; str[i] != '\0'; i++)
+        printf("%c\n", str[i]);
+    return 0;
+}
