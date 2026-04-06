08a73e3bae443a8b13f8f05c2d84008a4ad96086
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-04-07 02:11:37 +0500

    feat: add a program to check voting eligibility using if-else

---
+++ C/02-conditional-statements/votingeligibility.c
@@ -0,0 +1,16 @@
+// Program to check voting eligibility based on age using if-else
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18)
+        printf("You are eligible to vote.\n");
+    else
+        printf("You are not eligible to vote.\n");
+
+    return 0;
+}
