96fbe9262dd1cd2fb6203db4ec605086bd52eddc
Muhammad Wali Raza <waleeraxa@gmail.com>
2026-04-07 02:10:22 +0500

    feat: add a program to demonstrate escape sequences

---
+++ C/01-basic/escapesequences.c
@@ -0,0 +1,11 @@
+// Program to demonstrate escape sequences in C
+
+#include <stdio.h>
+
+int main() {
+    printf("Line 1\nLine 2\n");
+    printf("Column1\tColumn2\n");
+    printf("She said, \"Hello!\"\n");
+    printf("Path: C:\\Users\\Student\n");
+    return 0;
+}
